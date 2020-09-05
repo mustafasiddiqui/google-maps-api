196f8b95f90fec41257725453d75c0af9cb2ece6
Mustafa Siddiqui <mustafasiddiqui79@gmail.com>
2020-09-04 23:06:45 -0500

    Add Procfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const Store = require('./api/models/store');
 const { response } = require('express');
 const GoogleMapsService = require('./api/services/googleMapsService');
